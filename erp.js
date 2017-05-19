e2598a516b971631454e335fe3944af95f69fdd5
Robin <robinraj055@gmail.com>
2017-05-19 23:15:11 +0530

    ERP interface for admins

---
+++ erp.js
@@ -0,0 +1,11 @@
+function show(name ) {
+	var x = document.getElementById(name);
+	if ( x.style.display === "none")
+	{
+		x.style.display = "block";
+	}
+	else
+	{
+		x.style.display = "none";
+	}
+}
